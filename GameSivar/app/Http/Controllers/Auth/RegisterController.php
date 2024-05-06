41235a25594bad6b8d6d338ed5b6b9a5639b5eac
Samuel Mejía <sf977996@gmail.com>
2024-05-05 22:56:53 -0600

    loginyregistro funsional

--- GameSivar/app/Http/Controllers/Auth/RegisterController.php
+++ GameSivar/app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
@@ -50,6 +50,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'birth' => ['required', 'date',],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -65,6 +68,9 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'lastname' => $data['lastname'],
+            'phone' => $data['phone'],
+            'birth' => $data['birth'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
