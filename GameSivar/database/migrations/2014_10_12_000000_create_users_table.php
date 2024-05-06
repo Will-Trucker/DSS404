365de7ca5ee44b294e701adbe0c5fdcf4e98a79b
Samuel Mejía <sf977996@gmail.com>
2024-05-05 19:41:59 -0600

    modificacion

--- GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
+++ GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('genero');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('contrasena');
+            $table->string('telefono');
+            $table->date('nacimineto');
             $table->rememberToken();
             $table->timestamps();
         });
