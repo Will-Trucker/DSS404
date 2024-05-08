1032e8867eed5e556d5d9bac566dc9f0b3e08e9f
Juanzpa <167049339+Juanzpaxe@users.noreply.github.com>
2024-05-08 07:54:25 +0200

    Admin

--- GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
+++ GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->date('birth');
             $table->string('phone');
+            $table->boolean('isAdmin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
