dbb4114f9bee35e1643781b6f7663e912a2b1eb8
Samuel Mejía <sf977996@gmail.com>
2024-05-07 21:21:47 -0600

    asd

--- GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
+++ GameSivar/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('birth');
-            $table->string('cellphone');
+            $table->string('phone');
             $table->rememberToken();
             $table->timestamps();
         });
